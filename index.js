8cb109466d17a989cac32706de0484eacd2e43a0
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-03-27 12:13:23 +0200

    Updating URI

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ userSchema.methods.validatePassword = function (password) {
 };
 
 //Integrating Mongoose with RESTAPI myFlix is the name od Database with movies and users
-mongoose.connect("process.env.CONNECTION_URI", {
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
